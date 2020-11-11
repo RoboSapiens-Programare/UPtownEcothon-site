3da0b6a2d36cd90b4172c1d2e3e37a7e9d7362c5
Negoitescu Miruna <miruru2003@gmail.com>
2020-11-11 19:09:07 +0200

    Prototip Cerc Meniu

--- home.php
+++ home.php
@@ -6,8 +6,51 @@
 		  <link rel="stylesheet" type="text/css" href="css/slideup.css">
 		  <link rel="stylesheet" type="text/css" href="css/sageata.css">
 	</head>
-	<body>
-		<div class="row">
+
+	<body style="background-color:beige">
+
+		<div class="meniu-full">
+		
+			<div class="cerc-meniu-TOP">
+				
+				<div  id="logo-meniu-TOP" >
+
+					<div id="logo-full">
+
+						<!-- <script> 
+							el=document.getElementById('logo-meniu-TOP') 
+							el.style.height=el.offsetWidth/2+"px"
+						</script> -->
+
+						<div id="sageata-meniu-TOP">
+							<img src="css/1sageataUTE.png" alt="logo arrow" style="width: inherit; height:inherit">
+						</div>
+
+						<div id="town-TOP">
+							<img src="css/1townUTE.png" alt="logo town" style="width: inherit; height:inherit">
+						</div>
+
+						<div id="cothon-TOP">
+							<img src="css/1cothonUTE.png" alt="logo cothon" style="width: inherit; height:inherit">
+						</div>
+
+					</div>
+
+				</div>
+				
+			</div>
+
+		</div>
+
+		<div class="meniu-scroll">
+			<div class="cerc-sageata">
+				<!-- <div class="sageata-meniu-scroll">
+					<img class="sageata" src="css/1sageataUTE.png" alt="logo arrow">
+				</div> -->
+			</div>
+		</div>
+
+		<!-- <div class="row">
 			<div id="sageata" class="column" style="flex: 10%; background-color: #123123">
 				<div id="menu" class="meniu">Meniu
 					<a href="">Alta pagina</a>
@@ -17,23 +60,27 @@
 		
 			<div id="banner" class="column" style="flex: 90%; background-color: #75B33E">Banner</div>
 
-		</div>
-
-		
+		</div> -->
 
 		<div id="content1" class="row">
+			
 			<div id="desc1" class="column" style="flex: 30vh; height: 80vh; background-color: #2A3151">Descriere primul content</div>
 			<div id="imagine1" class="column" style="flex: 70vh; height: 80vh"></div>
 		</div>
+		<br>
 		<div id="content2" class="row">
 			<div id="imagine2" class="column" style="flex: 70vh; height: 100vh"></div>
 			<div id="desc2" class="column" style="flex: 30vh; background-color: #AA3151; height: 100vh">Descriere al doilea content</div>
-		</div>
+		</div> 
 
-		<?php
+		<!-- <?php
 			// for ($x = 0; $x < 2; $x++){
 			// 	echo "<div id="content1">Content 1</div>";
 			// }
-		?>
+		?> -->
+
 	</body>
+
+		
+
 </html>
