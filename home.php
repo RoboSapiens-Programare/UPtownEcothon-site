4b54f4325e3d77084783b184d95c7a234d4754fc
Negoitescu Miruna <miruru2003@gmail.com>
2020-11-16 19:18:24 +0200

    Draft meniu full + josul paginii

--- home.php
+++ home.php
@@ -16,7 +16,9 @@
 
 		  <link rel="stylesheet" type="text/css" href="css/slideup.css">
 		  <link rel="stylesheet" type="text/css" href="css/sageata.css">
+		  <link rel="stylesheet" type="text/css" href="css/bottom.css">
 
+		  <link href="https://allfont.net/allfont.css?fonts=agency-fb-bold" rel="stylesheet" type="text/css" />
 		  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 
 	</head>
@@ -24,7 +26,7 @@
 	<body style="background-color:beige">
     
 		<div class="meniu-full" id="meniu-full" style="display:block;">
-			<div id="cerc-meniu-full">
+			<div id="cerc-logo-full">
 				<div  id="logo-meniu-full" >
 					<div id="sageata-meniu-TOP" z-index="3">
 						<img src="pictures/1sageataUTE.png" alt="logo arrow" style="width: inherit; height:inherit">
@@ -37,9 +39,20 @@
 					</div>
 				</div>
 			</div>
+
+			<div class="cerc-text">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Descriere Eveniment</a></li>
+				<li><a href="#">Despre Noi</a></li>
+				<li><a href="#">Inscriere</a></li>
+				<li><a href="#">Informatii</a></li>
+				<li><a href="#">Got A Problem?</a></li>
+				<li><a href="#">Our Sponsors</a></li>
+			</div>
+
 		</div>
     
-		<div class="meniu-scroll" style="display:block; position: fixed; z-index: 1;" >
+		<div class="meniu-scroll" style="display:none; position: fixed; z-index: 1;" >
 			<div id="cerc-sageata-scroll">
 				<div id="sageata-meniu-scroll" >
 					<img class="sageata" src="pictures/1sageataUTE.png" alt="logo arrow" style="height:100%; position:absolute; left: 15%">
@@ -81,17 +94,28 @@
 			?>
 		</div>
 
-		<div id="bottom-of-page" class="row">
+		<div id="bottom-of-page" class="row" style="overflow:hidden;">
 		
-			<div id="logouri" class="column" flex="60vw" height="30vw">
-				c
+			<div id="logouri" class="column" style="overflow:hidden; flex:70%; height:30vw">
+				<ul style="display: inline;">
+					<li style="border-right: 3px solid black;">
+						<img src="pictures/logo v3.png" alt="logo robosabiens" style="padding-right: 0px;">	
+						<img src="pictures/robosapiens copy usa.png" alt="text robosapiens" style="padding-left: 0px;">
+					</li>
+					<li><img src="pictures/logo-header-web.png" alt="FTC/Natie prin educatie/BRD" style="padding-left:1vw; border-right: none;"></li>
+				</ul>
 			</div>
 
-			<div id="contact" class="column" flex="40vw" height="30vw">
-				<div id="cerc-contact"> b </div>
-				<!-- <p>
-					Contact <br> Pagini <br> Login <br> Chestii
-				</p> -->
+			<div id="contact" class="column" style="overflow:hidden; flex:40%; height:30vw">
+				<div id="cerc-contact" ></div>
+
+				<div>
+					<p>
+						Contact <br> Pagini <br> Login <br> Chestii
+					</p>
+				</div>
+				
+				
 			</div>
 		
 		
