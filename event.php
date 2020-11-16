04d77fbdb6c7d6cc6c595f703cebfcc99f7e1e1b
XDead <damageq12@gmail.com>
2020-11-16 22:00:27 +0200

    Adaugat event $ foldere pentru elemente comune si pt content

---
+++ event.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title></title>
+
+        <link rel="stylesheet" type="text/css" href="css/sageata.css">
+        <link rel="stylesheet" type="text/css" href="css/bottom.css">
+
+        <link href="https://allfont.net/allfont.css?fonts=agency-fb-bold" rel="stylesheet" type="text/css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    </head>
+    
+    <body>
+        <?php include "elements/sageata.html" ?>    
+    </body>
+</html>
