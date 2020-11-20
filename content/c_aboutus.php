b2e7b654ff472478572352082e295b0271850176
XDead <damageq12@gmail.com>
2020-11-20 12:10:18 +0200

    Lucrat la aboutus.php Inceput lucrul la o librarie de tranzitii.

--- content/c_aboutus.php
+++ content/c_aboutus.php
@@ -1,7 +1,10 @@
 <?php
-    $__DescEchipa = ["Descriere Echipa" => "Donec dignissim velit a felis varius mollis. Fusce tincidunt pretium elit ac rutrum. Aliquam fringilla dictum purus porta posuere. Quisque gravida in nisl vitae porttitor. Donec suscipit elementum nisi, eu dictum enim hendrerit vitae. Suspendisse libero nunc, lobortis sed est condimentum, facilisis euismod nulla. Praesent bibendum nibh commodo ultricies eleifend. "];
+    $__DescEchipa = ["title" => "Descriere Echipa", 
+                    "content" => "Donec dignissim velit a felis varius mollis. Fusce tincidunt pretium elit ac rutrum. Aliquam fringilla dictum purus porta posuere. Quisque gravida in nisl vitae porttitor. Donec suscipit elementum nisi, eu dictum enim hendrerit vitae. Suspendisse libero nunc, lobortis sed est condimentum, facilisis euismod nulla. Praesent bibendum nibh commodo ultricies eleifend. "];
 
-    $__Contact = ["Contact" => "Aliquam volutpat eget metus at ultrices. Cras tincidunt augue sit amet pellentesque vulputate. Nullam rutrum sollicitudin velit, sit amet faucibus. "];
+    $__Contact = ["title" => "Contact",
+                "content" => "Aliquam volutpat eget metus at ultrices. Cras tincidunt augue sit amet pellentesque vulputate. Nullam rutrum sollicitudin velit, sit amet faucibus. "];
 
-    $__ThisSite = ["This Site" => "Vivamus sollicitudin aliquam tincidunt. Phasellus eget venenatis justo, ac tempus justo. Duis dui ligula, aliquam at condimentum sed, gravida a massa. Pellentesque tristique ut felis in auctor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam et suscipit velit. Nulla odio felis, sollicitudin sit amet luctus nec, pulvinar rutrum lacus. Integer vestibulum vel libero ut ornare. Vestibulum laoreet erat ac sapien posuere tincidunt. Nam varius in lacus eu eleifend. "];
+    $__ThisSite = ["title" => "This Site",
+                "content" => "Vivamus sollicitudin aliquam tincidunt. Phasellus eget venenatis justo, ac tempus justo. Duis dui ligula, aliquam at condimentum sed, gravida a massa. Pellentesque tristique ut felis in auctor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam et suscipit velit. Nulla odio felis, sollicitudin sit amet luctus nec, pulvinar rutrum lacus. Integer vestibulum vel libero ut ornare. Vestibulum laoreet erat ac sapien posuere tincidunt. Nam varius in lacus eu eleifend. "];
 ?>
