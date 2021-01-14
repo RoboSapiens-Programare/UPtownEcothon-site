97c698292e017d4a5d2f050bbecf33fcbd399246
XDead <damageq12@gmail.com>
2021-01-15 00:06:45 +0200

    Iar incepem cu small changes-urile pana rezolv encoding-ul din phpMyAdmin

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -6,7 +6,7 @@
 
     class SQLiDB extends PDO {
         function __construct(){
-            parent::__construct($GLOBALS['dbtype'] . ':' . $GLOBALS['reg_db_name'], $GLOBALS['usr'], $GLOBALS['pass'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET UTF8"));
+            parent::__construct($GLOBALS['dbtype'] . ':' . $GLOBALS['reg_db_name'], $GLOBALS['usr'], $GLOBALS['pass'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8; SET CHARACTER SET UTF8"));
 
             $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             if($GLOBALS['dbtype'] == "sqlite")
@@ -16,7 +16,7 @@
 
     class ContentDB extends PDO {
         function __construct(){
-            parent::__construct($GLOBALS['dbtype'] . ':' . $GLOBALS['content_db_name'], $GLOBALS['usr'], $GLOBALS['pass'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET UTF8"));
+            parent::__construct($GLOBALS['dbtype'] . ':' . $GLOBALS['content_db_name'], $GLOBALS['usr'], $GLOBALS['pass'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8; SET CHARACTER SET UTF8"));
 
             $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
