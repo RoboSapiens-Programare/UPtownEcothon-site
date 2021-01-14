ed81c62afb6149161cb7110535afd7d730fb7a50
XDead <damageq12@gmail.com>
2021-01-15 00:20:16 +0200

    Inca unu :)

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -16,7 +16,7 @@
 
     class ContentDB extends PDO {
         function __construct(){
-            parent::__construct($GLOBALS['dbtype'] . ':' . $GLOBALS['content_db_name'], $GLOBALS['usr'], $GLOBALS['pass']);
+            parent::__construct($GLOBALS['dbtype'] . ':' . $GLOBALS['content_db_name'], $GLOBALS['usr'], $GLOBALS['pass'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
             $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
@@ -24,7 +24,7 @@
                 $this->exec("PRAGMA foreign_keys = ON");
             }
             else{
-                $this->exec("SET NAMES UTF8; SET CHARACTER SET UTF8;");
+                $this->exec("SET CHARACTER SET UTF8;");
             }
         }
 
