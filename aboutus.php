83dc44691811c92a85132eb24994384b3af33dfd
XDead <damageq12@gmail.com>
2021-01-16 13:07:48 +0200

    Added credits si removed mobile detect master

--- aboutus.php
+++ aboutus.php
@@ -40,6 +40,73 @@
                 height: inherit;
                 width:inherit;
             }
+
+             /* Popup container */
+            .popup {
+                position: relative;
+                display: inline-block;
+                cursor: pointer;
+                position: absolute; 
+                bottom: 10%; 
+                right: 10%; 
+                font-family: 'Khand', sans-serif; 
+                font-size: 1.3vw; 
+                font-weight: bold; 
+                color: white;
+            }
+
+            .popup:hover{
+                color: grey;
+            }
+
+            /* The actual popup (appears on top) */
+                .popup .popuptext {
+                visibility: hidden;
+                width: 260px;
+                background-color: #555;
+                color: #fff;
+                text-align: center;
+                border-radius: 6px;
+                padding: 8px 0;
+                position: absolute;
+                z-index: 1;
+                bottom: 125%;
+                left: 50%;
+                margin-left: -130px;
+                font-family: 'Montserrat', sans-serif;
+                font-weight: 300;
+                font-size: 1vw;
+            }
+
+            /* Popup arrow */
+            .popup .popuptext::after {
+                content: "";
+                position: absolute;
+                top: 100%;
+                left: 50%;
+                margin-left: -5px;
+                border-width: 5px;
+                border-style: solid;
+                border-color: #555 transparent transparent transparent;
+            }
+
+            /* Toggle this class when clicking on the popup container (hide and show the popup) */
+            .popup .show {
+                visibility: visible;
+                -webkit-animation: fadeIn 1s;
+                animation: fadeIn 1s
+            }
+
+            /* Add animation (fade in the popup) */
+            @-webkit-keyframes fadeIn {
+                from {opacity: 0;}
+                to {opacity: 1;}
+            }
+
+            @keyframes fadeIn {
+                from {opacity: 0;}
+                to {opacity:1 ;}
+            } 
         </style>
     </head>
 
@@ -188,6 +255,22 @@
             <div id="bigtitle" class="btitle"><?php echo $content['Site']['Title'] ?></div>
             <div id="bcontent" class="bcontent"><?php echo $content['Site']['Long'] ?></div>
             
+            <div class="popup" onclick="showCredits()">Show credits
+                <span class="popuptext" id="credPopup">
+                    <ul>
+                        <?php 
+                            //Acest cod cred ca e cel mai prost mod de a face ce voiam sa fac :D
+                            $thesmecheri = array("Negoițescu Miruna", "Tanislav Alexia", "Peter Daniel");
+                            $rand_keys = array_rand($thesmecheri, 2);
+                            foreach ($rand_keys as $a){
+                                echo "<li>" . $thesmecheri[$a] . "</li>";
+                                unset($thesmecheri[$a]);
+                            }
+                            echo "<li>" . array_slice($thesmecheri, 0, 3)[0] . "</li>";
+                        ?>
+                    </ul>
+                </span>
+            </div>
         </div>
         <div class="bigcontent" style="opacity: 1;">
             <div class="btitle" style="font-size: 5vw; width:auto; top: 50%; left: 50%; transform: translate(-50%, -50%)">
@@ -226,6 +309,11 @@
                         transitions.fadeIn(smallcontents[i], tweenFunctions.easeOutExpo, 1000);
                     }
                 }
+            }
+
+            function showCredits(){
+                var popup = document.getElementById("credPopup");
+                popup.classList.toggle("show");
             }
 
             grecaptcha.ready(function() {
