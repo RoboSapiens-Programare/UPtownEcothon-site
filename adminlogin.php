3942c0f4b3a06166d3994d9679c50513da773fcf
XDead <damageq12@gmail.com>
2021-02-10 22:33:30 +0200

    This is utterly retarded

--- adminlogin.php
+++ adminlogin.php
@@ -13,9 +13,14 @@
     require_once('config/captchacredentials.php');
     require_once('config/captchaconfig.php');  
 
-    $res = verify_captcha();
+    // if(isset($_POST['token']) && !empty($_POST['token'])){
+    //     $res = verify_captcha();
+    // }
+    // else{
+    //     $res = null;
+    // }
     
-    if ($res['success'] == true && $res['score'] >= 0.5) {
+    // if ($res && $res['success'] == true && $res['score'] >= 0.5) {
         
         require_once 'config/dbconfig.php';
 
@@ -63,12 +68,12 @@
             echo $e->getMessage();
         }
     
-    } else {
+    // } else {
     
-        echo '<div>
-        Error! The security token has expired or you are a bot.
-        </div>';
-    }  
+    //     echo '<div>
+    //     Error! The security token has expired or you are a bot.
+    //     </div>';
+    // }  
 ?>
 
 <html>
